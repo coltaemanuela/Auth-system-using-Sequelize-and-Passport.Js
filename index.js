e239dcb40dab3601c76d3f09d70e7dd9e3c2881d
Colta Emanuela <coltaemanuela@gmail.com>
2017-03-15 14:57:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,27 +10,29 @@ var passportsession= require('passport-session');
 
 
 passport.use(new LocalStrategy(
-  function(email, password, done) {
-    User.findOne({ email: email }, function (err, user) {
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
       if (err) { return done(err); }
       if (!user) {
-        return done(null, false);
+        return done(null, false, { message: 'Incorrect username.' });
       }
-      if (user.password != password) {
-        return done(null, false);
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
     });
   }
 ));
 
+
 passport.serializeUser(function(user, done) {
-  done(null, user.userid);
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(userid, done) {
-  User.findById(userid, function(err, user) {
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
     done(err, user);
+    console.log(id);
   });
 });
 
