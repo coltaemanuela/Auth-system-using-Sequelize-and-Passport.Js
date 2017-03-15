904940512df152b80e886754f93e54e4e182f100
Ionică Bizău <bizauionica@gmail.com>
2017-03-15 17:40:41 +0200

    Store data in the session

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var passportlocal= require('passport-local');
-var passportsession= require('passport-session');
+//var LocalStrategy = require('passport-session').Strategy
 var User = require('./models/users_model.js');
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
+passport.use(new LocalStrategy({
+    passReqToCallback: true
+}, function(req, username, password, done) {
     User.findOne({ username: username }).then(function(user) {
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
@@ -18,10 +19,11 @@ passport.use(new LocalStrategy(
       if (password !== user.password) {
         return done(null, false, { message: 'Incorrect password.' });
       }
+      req.session.user = user.toJSON();
       //if (!user.validPassword(password)) {
       //  return done(null, false, { message: 'Incorrect password.' });
       //}
-      done(null, user.toJSON());
+      done(null, req.session.user);
     }).catch(err => {
       console.error(err);
       done(err);
@@ -35,15 +37,24 @@ var router = express.Router();
 
 var app = express();
 
+app.use(session({
+    secret:"ceva",
+    resave:true, //false
+    saveUninitialized:true,
+    cookie:{},
+    duration: 45 * 60 * 1000,
+    activeDuration: 15 * 60 * 1000
+}));
+
 passport.serializeUser(function(user, done) {
   done(null, user.userid);
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findOne({ userid: id }, function(err, user) {
-    done(err, user);
+  User.findOne({ userid: id }).then(function(user) {
+    done(null, user);
     console.log(id);
-  });
+  }).catch(e => done(e));
 });
 // Passport init
 app.use(passport.initialize());
@@ -60,19 +71,9 @@ app.use(bodyParser.json());
 
 app.use('/users', users);
 app.get("/session", (req, res) => {
-    debugger
     res.json(req.session);
 });
 
-
-app.use(session({
-    secret:"ceva",
-    resave:true, //false
-    saveUninitialized:true,
-    cookie:{},
-    duration: 45 * 60 * 1000,
-    activeDuration: 15 * 60 * 1000
-}));
 
 
 //------------------------------------------------Routes---------------------------------------------------------------------------
